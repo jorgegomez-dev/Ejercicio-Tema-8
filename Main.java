2f6453633bf954a4f4ca00d29cb4fa0dc857b30c
jorgegomezdi <106641101+jorgegomezdi@users.noreply.github.com>
2022-06-10 22:37:14 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,41 @@
+public class Main {
+    public static void main(String[] args) {
+        Persona persona1 = new Persona();
+        persona1.setEdad(21);
+        persona1.setNombre("Juan");
+        persona1.setTelefono(1144645898);
+        System.out.println("Nombre: " + persona1.getNombre());
+        System.out.println("Edad: " + persona1.getEdad());
+        System.out.println("Telefono: " + persona1.getTelefono());
+    }
+
+    public static class Persona{
+        private int edad;
+        private String nombre;
+        private int telefono;
+
+        public int getEdad() {
+            return edad;
+        }
+
+        public void setEdad(int edad) {
+            this.edad = edad;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public int getTelefono() {
+            return telefono;
+        }
+
+        public void setTelefono(int telefono) {
+            this.telefono = telefono;
+        }
+    }
+}
